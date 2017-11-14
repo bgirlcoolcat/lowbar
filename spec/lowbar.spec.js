f1865f045b71179e9f2c1e00713aa45b7e59f84c
bgirlcoolcat <evansb21@gmail.com>
2017-11-14 13:03:29 +0000

    lowbar project set up complete

---
+++ spec/lowbar.spec.js
@@ -0,0 +1,13 @@
+var path = require('path');
+var expect = require('chai').expect;
+
+var _ = require(path.join(__dirname, '..', './lowbar.js'));
+
+describe('_', function () {
+  'use strict';
+
+  it('is an object', function () {
+    expect(_).to.be.an('object');
+  });
+  
+});
