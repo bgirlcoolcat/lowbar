32f7fc87c58de0658a497da9d564f05ba7e54b08
bgirlcoolcat <evansb21@gmail.com>
2017-11-14 17:01:01 +0000

    [_.first] returns the first element of an array

--- lowbar.js
+++ lowbar.js
@@ -5,4 +5,14 @@ _.identity = function (value) {
   return value;
 };
 
+// FIRST
+_.first = function (array) {
+    if (!(Array.isArray(array)) && typeof array !== 'string') { 
+      return undefined; 
+    } else {
+      return array[0];
+    }
+};
+
+
 module.exports = _;
