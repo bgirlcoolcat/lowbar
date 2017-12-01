af0eb3f3d54da92c67f590693918126c2bcb1fa8
bgirlcoolcat <evansb21@gmail.com>
2017-12-01 16:09:59 +0000

    [_.each] refactor to take a context argument

--- lowbar.js
+++ lowbar.js
@@ -128,11 +128,13 @@ _.reject = function (list, predicate) {
 };
 
 // EACH
-_.each = function (list, iteratee) {
+_.each = function (list, iteratee, context) {
+  if (context) {
+    iteratee = iteratee.bind(context);
+  }
   if (!iteratee) {
     return list;
   }
-
   if (Array.isArray(list) || typeof list === 'string') {
       for (var i = 0; i < list.length; i++) {
         iteratee(list[i], i, list);
