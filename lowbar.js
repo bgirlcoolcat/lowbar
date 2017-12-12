375a9fd8fa2d2370edb2d29149b287cc5de9cbba
bgirlcoolcat <evansb21@gmail.com>
2017-12-12 16:38:28 +0000

    [_.shuffle] returns a shuffled copy of the list, using a modern version of the Fisher-Yates shuffle

--- lowbar.js
+++ lowbar.js
@@ -302,5 +302,16 @@ _.negate = function (predicate) {
   };
 };
 
-  
+// SHUFFLE
+_.shuffle = function (list) {
+  let j, x, i;
+  for (i = list.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = list[i];
+      list[i] = list[j];
+      list[j] = x;
+  }
+  return list;
+};
+
 module.exports = _;
